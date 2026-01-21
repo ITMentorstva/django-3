e773445eba309c898e9b51da42b5f8e7689597c4
FasterThanIE <tomicawarr@gmail.com>
2026-01-21 11:19:24 +0100

    * Finished lecture II

--- core/views.py
+++ core/views.py
@@ -1,17 +1,53 @@
 
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 
-# Postoji funkcija home koja prikazuje nekakvu poruku "Hello world"
-# Gde se prikazuje ova funkcija?
+products = {
+        "Macbook Air 2025": {
+            "price": 2000,
+            "description": "This is Macbook Air 2025"
+        },
+        "Macbook Pro 2025": {
+            "price": 2800,
+            "description": "This is Macbook Pro 2025"
+        },
+        "iPhone 16 Pro": {
+            "price": 1500,
+            "description": "This is iPhone 16 Pro"
+        },
+        "iPad Pro 2025": {
+            "price": 1800,
+            "description": "This is iPad Pro 2025"
+        },
+        "Apple Watch Ultra 2": {
+            "price": 900,
+            "description": "This is Apple Watch Ultra 2"
+        }
+    }
+
+
 def home(request):
-    return render(request, 'index.html', status=200)
+
+    context = {
+        "products": products
+    }
+
+    return render(request, 'index.html', context)
 
 def about(request):
     return HttpResponse("An error happened while serving this page", status=404)
 
 def product(request, name):
-    return HttpResponse(f'This is {name}')
+
+    product = products.get(name)
+    if not product:
+        return HttpResponseNotFound("Proizvod koji trazite nije pronadjen!")
+
+    context = {
+        "product": product
+    }
+
+    return render(request, 'product.html', context)
 
 def user(request, userId):
     return HttpResponse(f'This is user {userId}')
